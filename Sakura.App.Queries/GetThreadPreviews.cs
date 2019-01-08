30fe35d185855f4f0b693889243b39c048a0727f
Nina Lisitsinskaya <vvatashi4259@gmail.com>
2019-01-09 00:42:07 +0700

    Add op-post fields to thread previews

--- Sakura.App.Queries/GetThreadPreviews.cs
+++ Sakura.App.Queries/GetThreadPreviews.cs
@@ -21,26 +21,49 @@ namespace Sakura.App.Queries
         var connection = ConnectionFactory.GetConnection();
         string sql = @"
 SELECT
-  t.thread_id as threadId,
-  t.bumped_at as bumpedAt,
+  t.thread_id as thread_id,
+  t.bumped_at as bumped_at,
   p.post_id as id,
-  p.view_id as viewId,
+  p.view_id as view_id,
   p.message as message,
-  p.created_at as createdAt
+  p.created_at as created_at,
+  r.post_id as reply_id,
+  r.view_id as reply_view_id,
+  r.message as reply_message,
+  r.created_at as reply_created_at
 FROM threads AS t
+JOIN LATERAL (
+  SELECT *
+  FROM posts AS p
+  WHERE p.thread_id = t.thread_id
+  ORDER BY p.view_id
+  LIMIT 1
+) AS p ON p.thread_id = t.thread_id
 JOIN LATERAL (
   SELECT *
   FROM posts AS p
   WHERE p.thread_id = t.thread_id
   ORDER BY p.view_id DESC
   LIMIT 3
-) p ON p.thread_id = t.thread_id
-ORDER  BY t.bumped_at DESC, p.view_id";
+) AS r ON r.thread_id = t.thread_id
+ORDER BY t.bumped_at DESC, r.view_id";
         var threads = new Dictionary<Guid, ThreadPreviewViewModel>();
         foreach (dynamic row in connection.Query(sql)) {
-          var post = new PostViewModel(row.threadid, row.id, row.viewid, row.message, row.createdat);
+          var post = new PostViewModel(
+            row.thread_id,
+            row.reply_id,
+            row.reply_view_id,
+            row.reply_message,
+            row.reply_created_at);
           if (!threads.TryGetValue(post.ThreadId, out var thread)) {
-            threads.Add(post.ThreadId, thread = new ThreadPreviewViewModel(post.ThreadId, row.bumpedat));
+            thread = new ThreadPreviewViewModel(
+              post.ThreadId,
+              row.id,
+              row.view_id,
+              row.message,
+              row.created_at,
+              row.bumped_at);
+            threads.Add(post.ThreadId, thread);
           }
 
           thread.Posts.Add(post);
--- Sakura.App.Queries/ThreadPreviewViewModel.cs
+++ Sakura.App.Queries/ThreadPreviewViewModel.cs
@@ -6,14 +6,26 @@ namespace Sakura.App.Queries
   public class ThreadPreviewViewModel
   {
     public readonly Guid ThreadId;
+    public readonly Guid Id;
+    public readonly long ViewId;
+    public readonly string Message;
+    public readonly DateTime CreatedAt;
     public readonly DateTime BumpedAt;
     public readonly List<PostViewModel> Posts = new List<PostViewModel>();
 
     public ThreadPreviewViewModel(
       Guid threadId,
+      Guid id,
+      long viewId,
+      string message,
+      DateTime createdAt,
       DateTime bumpedAt)
     {
       ThreadId = threadId;
+      Id = id;
+      ViewId = viewId;
+      Message = message;
+      CreatedAt = createdAt;
       BumpedAt = bumpedAt;
     }
   }
